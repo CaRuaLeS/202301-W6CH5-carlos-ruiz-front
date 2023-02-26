81c420c44e9cded27aa30c1dfc2141e983ca27e2
Carlos <carua97@gmail.com>
2023-02-26 22:43:47 +0100

    Add app test and header

---
+++ src/hooks/use.fruits.test.tsx
@@ -0,0 +1,12 @@
+describe("Given the App component", () => {
+  const mockFruitsRepo = {
+    readFruits: jest.fn(),
+    readOneFruits: jest.fn(),
+    create: jest.fn(),
+    update: jest.fn(),
+    delete: jest.fn(),
+  };
+  describe("When it renders", () => {
+    test("Then it is in the document", () => {});
+  });
+});
