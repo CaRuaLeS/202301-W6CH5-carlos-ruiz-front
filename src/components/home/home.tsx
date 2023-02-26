a61d13a4cf95eddcd22634f080689e6c207e25bc
Carlos <carua97@gmail.com>
2023-02-26 22:25:47 +0100

    add home functionality and css

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,7 +1,24 @@
+import { useMemo } from "react";
+import { useFruits } from "../../hooks/use.fruits";
+import { FruitsRepo } from "../../services/api.fruits.repo";
+import { Card } from "../card/card";
+
+import "./home.css";
+
 export function Home() {
+  const repo = useMemo(() => new FruitsRepo(), []);
+  const { fruits } = useFruits(repo);
+
+  console.log(fruits.fruits);
+
   return (
     <>
-      <h1>Principal</h1>
+      <h1>List of fruits:</h1>
+      <ul className="fruit-list">
+        {fruits.fruits.map((item) => {
+          return <Card key={item.id} fruit={item}></Card>;
+        })}
+      </ul>
       <p></p>
     </>
   );
